d6b0de6f120d57829fe5c63b647d462a61f307e6
hg1g <hernan@lawal.coop>
2026-01-23 18:22:44 -0300

    Fix email constraint in setup-test-users

--- scripts/setup-test-users.sh
+++ scripts/setup-test-users.sh
@@ -31,7 +31,7 @@ setup_dronetm() {
     # Ivan Gayton (27 proj) → Andrea
     # Primero limpiamos los emails target si ya existen en otros users, luego asignamos
     docker exec hotosm-dronetm-db psql -U dtm -d dtm_db -c "
-    UPDATE users SET email_address = NULL WHERE email_address IN ('$HERNAN_EMAIL', '$JUSTINA_EMAIL', '$ANDREA_EMAIL');
+    UPDATE users SET email_address = 'old_' || email_address WHERE email_address IN ('$HERNAN_EMAIL', '$JUSTINA_EMAIL', '$ANDREA_EMAIL');
     UPDATE users SET email_address = '$HERNAN_EMAIL' WHERE email_address = 'philip.hippolyte@hotosm.org';
     UPDATE users SET email_address = '$JUSTINA_EMAIL' WHERE email_address = 'juan.melo@hotosm.org';
     UPDATE users SET email_address = '$ANDREA_EMAIL' WHERE email_address = 'ivan.gayton@hotosm.org';
